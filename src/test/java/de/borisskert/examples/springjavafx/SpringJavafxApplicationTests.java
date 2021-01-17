4e48b549fb32bef638c83364de4b86c834127e91
Boris Skert <boris.skert@gmail.com>
2021-01-17 11:13:40 +0100

    Create project with Spring Initializr

---
+++ src/test/java/de/borisskert/examples/springjavafx/SpringJavafxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.examples.springjavafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavafxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
